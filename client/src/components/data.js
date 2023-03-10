96c3be9d289be237c333a298faf161b25d08a7fa
Abdullahi Ahmad <beenai@Apples-MBP.lan>
2023-03-10 14:00:37 +0100

    configuring CI CD

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,11 +1,11 @@
 export const data = [
   {
     id: 1,
-    imagepath: 'assets/img/projects/core-working.jpg',
-    title: 'Core-working Space',
-    desc: '    is simply dummy text of the printing and typesetting industry.Lorem',
-    demo: 'https://github.com/abunnazeer',
-    github: 'https://github.com/abunnazeer',
+    imagepath: 'assets/img/projects/eduline.png',
+    title: 'Eduline',
+    desc: 'Eduline is school management system that inlude online class, exam system, and result computing',
+    demo: 'https://abunnazeer.com.ng/projects/frontend/eduline/',
+    github: 'https://github.com/abunnazeer/eduline',
   },
   {
     id: 2,
